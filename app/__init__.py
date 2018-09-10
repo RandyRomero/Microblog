9c72c548e6b55ea341a3c97690a56c31512a3d38
RandyRomero <a.ya.miheev@gmail.com>
2018-09-10 09:04:21 +0300

    Add logger for console

--- app/__init__.py
+++ app/__init__.py
@@ -48,11 +48,17 @@ if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(stream_handler)
+
+    app.logger.setLevel(logging.DEBUG)
     app.logger.info('Microblog startup')
 
 
